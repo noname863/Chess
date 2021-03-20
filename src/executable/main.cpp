17ed5a80306c7d32bdb4e4af30678f08d89e4bf5
noname863 <pawel00@mail.ru>
2021-03-20 13:15:24 +0300

    created swapchain

--- src/executable/main.cpp
+++ src/executable/main.cpp
@@ -79,23 +79,81 @@ vk::UniqueInstance createVkInstance()
     return std::move(vkInstance);
 }
 
-// int rateDevice(vk::PhysicalDevice& device)
-// {
-// }
+std::vector<const char *> initListOfRequiredDeviceExtentions()
+{
+    std::vector<const char *> result{
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+    std::sort(result.begin(), result.end());
+    return result;
+}
 
-// bool deviceSuitable(vk::PhysicalDevice& device)
-// {
-//     bool result = true;
-//     auto properties = device.getQueueFamilyProperties();
-//     result &= std::find_if(properties.begin(), properties.end(), 
-//             [](auto& queueFamily)
-//             {
-//                 return queueFamily.queueFlags & vk::QueueFlagBits::eGraphics;
-//             }) != properties.end();
-//     // possible another checks
-// 
-//     return result;
-// }
+const std::vector<const char *> requiredDeviceExtentions =
+    initListOfRequiredDeviceExtentions();
+
+bool isRequiredDeviceExtentionsSupported(vk::PhysicalDevice physicalDevice)
+{
+    auto[getExtentionsResult, extentions] = physicalDevice.enumerateDeviceExtensionProperties();
+    criticalVulkanAssert(getExtentionsResult, "error enumerating device extentions");
+    std::vector<const char *> extentionNames;
+    extentionNames.reserve(extentions.size());
+    for (auto& extention : extentions)
+    {
+        extentionNames.push_back(extention.extensionName);
+    }
+    return std::search(extentionNames.begin(), extentionNames.end(),
+            requiredDeviceExtentions.begin(), requiredDeviceExtentions.end()) != 
+            extentionNames.end();
+}
+
+std::optional<FamilyIndeces> getFamilyIndeces(vk::PhysicalDevice physicalDevice, vk::SurfaceKHR surface)
+{
+    auto queueFamiliesProperties = physicalDevice.getQueueFamilyProperties();
+    uint32_t indexSurfaceSupported = std::numeric_limits<uint32_t>::max();
+    uint32_t indexGraphicsSupported = std::numeric_limits<uint32_t>::max();
+    for (uint32_t i = 0; i < queueFamiliesProperties.size(); ++i)
+    {
+        auto[result, value] = physicalDevice.getSurfaceSupportKHR(i, surface);
+        criticalVulkanAssert(result, "error in checking support of surface in physical device");
+        const bool surfaceSupported = value;
+        const bool graphicsSupported = 
+            (queueFamiliesProperties[i].queueFlags & vk::QueueFlagBits::eGraphics) ==
+            vk::QueueFlagBits::eGraphics; 
+        if (surfaceSupported && graphicsSupported)
+        {
+            return FamilyIndeces{i};
+        }
+        else
+        {
+            if (surfaceSupported)
+            {
+                indexSurfaceSupported = i;
+            }
+            if (graphicsSupported)
+            {
+                indexGraphicsSupported = i;
+            }
+        }
+    }
+    if (indexGraphicsSupported < queueFamiliesProperties.size() &&
+        indexSurfaceSupported < queueFamiliesProperties.size())
+    {
+        return FamilyIndeces{indexGraphicsSupported, indexSurfaceSupported};
+    }
+    else
+    {
+        return std::nullopt;
+    }
+}
+
+bool surfaceAndSwapChainCompatible(vk::PhysicalDevice physicalDevice, vk::SurfaceKHR& surface)
+{
+    auto[getFormatsResult, surfaceFormats] = physicalDevice.getSurfaceFormatsKHR(surface);
+    criticalVulkanAssert(getFormatsResult, "error getting surface formats");
+    auto[getPresentModesResult, presentModes] = physicalDevice.getSurfacePresentModesKHR(surface);
+    criticalVulkanAssert(getPresentModesResult, "error getting present modes");
+    return !surfaceFormats.empty() && !presentModes.empty();
+}
 
 std::pair<vk::PhysicalDevice, FamilyIndeces>
 pickPhysicalDeviceAndQueueFamily(vk::Instance& instance, vk::SurfaceKHR& surface)
@@ -105,40 +163,21 @@ pickPhysicalDeviceAndQueueFamily(vk::Instance& instance, vk::SurfaceKHR& surface
     fassert(physicalDevices.size() != 0, "no physical devices found");
     for (auto& physicalDevice : physicalDevices)
     {
-        std::cout << physicalDevices.front().getProperties().deviceName << std::endl;
-        auto queueFamiliesProperties = physicalDevice.getQueueFamilyProperties();
-        bool surfaceSupported;
-        bool graphicsSupported;
-        uint32_t indexSurfaceSupported = std::numeric_limits<uint32_t>::max();
-        uint32_t indexGraphicsSupported = std::numeric_limits<uint32_t>::max();
-        for (uint32_t i = 0; i < queueFamiliesProperties.size(); ++i)
+        if (!isRequiredDeviceExtentionsSupported(physicalDevice))
         {
-            auto[result, value] = physicalDevice.getSurfaceSupportKHR(i, surface);
-            criticalVulkanAssert(result, "error in checking support of surface in physical device");
-            surfaceSupported = value;
-            graphicsSupported = 
-                (queueFamiliesProperties[i].queueFlags & vk::QueueFlagBits::eGraphics) ==
-                vk::QueueFlagBits::eGraphics; 
-            if (surfaceSupported && graphicsSupported)
+            continue;
+        }
+        else
+        {
+            if (!surfaceAndSwapChainCompatible(physicalDevice, surface))
             {
-                return {physicalDevice, FamilyIndeces({i})};
-            }
-            else
-            {
-                if (surfaceSupported)
-                {
-                    indexSurfaceSupported = i;
-                }
-                if (graphicsSupported)
-                {
-                    indexGraphicsSupported = i;
-                }
+                continue;
             }
         }
-        if (indexGraphicsSupported < queueFamiliesProperties.size() &&
-            indexSurfaceSupported < queueFamiliesProperties.size())
+        auto optionalFamilyIndeces = getFamilyIndeces(physicalDevice, surface);
+        if (optionalFamilyIndeces.has_value())
         {
-            return {physicalDevice, FamilyIndeces{indexGraphicsSupported, indexSurfaceSupported}};
+            return {physicalDevice, optionalFamilyIndeces.value()};
         }
     }
     fassert(false, "no sutable device found");
@@ -156,10 +195,93 @@ vk::UniqueDevice createDevice(vk::PhysicalDevice physicalDevice, FamilyIndeces q
     }
     // default zero
     vk::PhysicalDeviceFeatures deviceFeatures{};
-    vk::DeviceCreateInfo deviceCreateInfo({}, queueInfos.size(), queueInfos.data());
+    vk::DeviceCreateInfo deviceCreateInfo({}, queueInfos.size(), queueInfos.data(), 
+            0, nullptr, requiredDeviceExtentions.size(), requiredDeviceExtentions.data());
     auto[result, device] = physicalDevice.createDeviceUnique(deviceCreateInfo);
     criticalVulkanAssert(result, "failed to create logical device");
     return std::move(device);
+}
+
+vk::SurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<vk::SurfaceFormatKHR>& formats)
+{
+    for (auto& format : formats)
+    {
+        if (format.format == vk::Format::eB8G8R8A8Srgb && 
+                format.colorSpace == vk::ColorSpaceKHR::eVkColorspaceSrgbNonlinear)
+        {
+            return format;
+        }
+    }
+    return formats.front();
+}
+
+vk::PresentModeKHR chooseSwapPresentMode(const std::vector<vk::PresentModeKHR>& presentModes)
+{
+    if (std::find(presentModes.begin(), presentModes.end(), vk::PresentModeKHR::eMailbox) != presentModes.end())
+    {
+        return vk::PresentModeKHR::eMailbox;
+    }
+    if (std::find(presentModes.begin(), presentModes.end(), vk::PresentModeKHR::eFifoRelaxed) != presentModes.end())
+    {
+        return vk::PresentModeKHR::eFifoRelaxed;
+    }
+    return vk::PresentModeKHR::eFifo;
+}
+
+vk::Extent2D createNewExtent2D(vkfw::Window& window, const vk::SurfaceCapabilitiesKHR capabilities)
+{
+        auto [result, sizeTuple] = window.getFramebufferSize();
+        auto [width, height] = sizeTuple;
+        vk::Extent2D resultExtent;
+        resultExtent.width = std::clamp(static_cast<uint32_t>(width), 
+                capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+        resultExtent.height = std::clamp(static_cast<uint32_t>(height), 
+                capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+        return resultExtent;
+}
+
+vk::Extent2D getSwapExtent2D(vkfw::Window& window, const vk::SurfaceCapabilitiesKHR capabilities)
+{
+    if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+    {
+        return capabilities.currentExtent;
+    }
+    else
+    {
+        return createNewExtent2D(window, capabilities);
+    }
+}
+
+vk::UniqueSwapchainKHR createSwapChain(
+        vk::PhysicalDevice& physicalDevice, vk::Device& device, FamilyIndeces familyIndeces,
+        vkfw::Window& window, vk::SurfaceKHR& surface)
+{
+    auto[getCapabilitiesResult, capabilities] =
+        physicalDevice.getSurfaceCapabilitiesKHR(surface);
+    criticalVulkanAssert(getCapabilitiesResult, "error receiving capabilities");
+    uint32_t minImageCount = capabilities.minImageCount;
+    if (capabilities.maxImageCount == 0 ||
+        capabilities.minImageCount != capabilities.maxImageCount)
+    {
+        ++minImageCount;
+    }
+    auto [getSurfaceFormatsResult, surfaceFormats] = 
+        physicalDevice.getSurfaceFormatsKHR(surface);
+    criticalVulkanAssert(getSurfaceFormatsResult, "error getting surface formats");
+    vk::SurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(surfaceFormats);
+    auto [getPresentModesResult, presentModes] = 
+        physicalDevice.getSurfacePresentModesKHR(surface);
+    criticalVulkanAssert(getPresentModesResult, "error getting present modes");
+    vk::PresentModeKHR presentMode = chooseSwapPresentMode(presentModes);
+    vk::Extent2D extent = getSwapExtent2D(window, capabilities);
+    vk::SwapchainCreateInfoKHR createInfo({}, surface, minImageCount, 
+            surfaceFormat.format, surfaceFormat.colorSpace, extent, 1,
+            vk::ImageUsageFlagBits::eColorAttachment, vk::SharingMode::eExclusive,
+            static_cast<uint32_t>(familyIndeces.indexes.size()), familyIndeces.indexes.data(),
+            capabilities.currentTransform, vk::CompositeAlphaFlagBitsKHR::eOpaque, presentMode, true, {});
+    auto [createSwapChainResult, swapChain] = device.createSwapchainKHRUnique(createInfo);
+    criticalVulkanAssert(createSwapChainResult, "failed to create swapchain");
+    return std::move(swapChain);
 }
 
 int main()
@@ -172,6 +294,8 @@ int main()
     auto [physicalDevice, queueFamilyIndeces] =
         pickPhysicalDeviceAndQueueFamily(instance.get(), surface.get());
     vk::UniqueDevice device = createDevice(physicalDevice, queueFamilyIndeces);
+    vk::UniqueSwapchainKHR swapchain = createSwapChain(physicalDevice, device.get(), 
+            queueFamilyIndeces, mainWindow.get(), surface.get());
 
     while (true)
     {
